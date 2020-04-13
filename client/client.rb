3644742257c64a8f2b89c0dc1b5f091837bd690d
JP <jp@sutrosoftware.com>
2020-04-12 22:19:53 -0700

    remove customized muni gem, use OOTB one instead

--- client/client.rb
+++ client/client.rb
@@ -7,37 +7,45 @@ require_relative 'lib'
 font = muni_sign_font(File.join(File.dirname(__FILE__), 'font'))
 
 options = {
-  :update_interval => 30,
+    :update_interval => 30,
 }
+
+ARGV << "--help" if ARGV.length < 3
+
 OptionParser.new do |opts|
-  opts.banner = "Usage: client.rb --route F --direction inbound --stop 'Ferry Building'"
-
-  opts.on('--stopId [STOP_NAME]', "Stop to watch") {|v| options[:stop] = v}
-  opts.on('--update-interval SECONDS', Integer, "Update sign each number of seconds") {|v| options[:update_interval] = v}
-
-  # Darkening
-  opts.on('--dark-file [FILENAME]', "Turn off the sign instead of updating, if FILENAME exists") {|v| options[:dark_file] = v}
+  opts.banner = "Usage: client.rb"
+  # Required
+  opts.on('--route [route]', "Route name or number") { |v| options[:route] = v }
+  opts.on('--dir [outbound|inbound]', "Route direction") { |v| options[:dir] = v }
+  opts.on('--stop [stop]', "Stop name or ID to watch") { |v| options[:stop] = v }
+  # Optional
+  opts.on('--update-interval [seconds]', Integer, "Sign updates per second (default 30)") { |v| options[:update_interval] = v }
+  opts.on('--dark-file [filename]', "Turn off the sign instead of updating, if filename exists") { |v| options[:dark_file] = v }
 end.parse!
 
 
 # Returns hash of predictions for this stop in UTC times for all routes.  Keys
 # are route names, and values are arrays of predictions for that route at this
 # stop.
-def get_stop_arrivals(stopId)
-  raise unless stopId
-  stop = Muni::Stop.new({ :stopId => stopId })
-  return stop.predictions_for_all_routes
+def get_stop_arrivals(route, direction, stop)
+  raise unless (route and direction and stop)
+  r = Muni::Route.find(route)
+  s = r.direction_at(direction).stop_at(stop)
+  retval = {}
+  retval[r['title']] = s.predictions
+  retval
 end
 
 # Convert from Nextbus format to what it actually displayed on a minu sign.
 # Ordered list of regexp -> string pairs.  The first regexp to match a
 # prediction's dirTag field replaces the route name with the string.
 ROUTE_FIXUP_MAP = [
-  [ /^KT.*OB/, 'K-Ingleside'],
-  [ /^KT.*IBMTME/, 'T-Metro East Garage'],
-  # Let's all all inbound KT-s like this.
-  [ /^KT.*IB/, 'T-Third Street'],
+    [/^KT.*OB/, 'K-Ingleside'],
+    [/^KT.*IBMTME/, 'T-Metro East Garage'],
+    # Let's all all inbound KT-s like this.
+    [/^KT.*IB/, 'T-Third Street'],
 ]
+
 def fixup_route_name(route_name, prediction)
   # For now, just truncate, except for one thing.
   unstripped_result = route_name
@@ -51,13 +59,27 @@ def fixup_route_name(route_name, prediction)
   unstripped_result.slice(0, 18)
 end
 
+def muni_time(time)
+  distance = time - Time.now
+  if distance > 60
+    "#{(distance / 60).to_i} min"
+  elsif distance < -10
+    # Not really useful.
+    "#{((-distance) / 60).to_i} min ago"
+  else
+    # Finally!  It took that damn train like forever!
+    "Arriving"
+  end
+end
+
 def update_sign(font, options)
-  arrival_times = get_stop_arrivals(options[:stop])
-  # Only debugging: $stderr.puts arrival_times.inspect
+  arrival_times = get_stop_arrivals(options[:route], options[:dir], options[:stop])
+  # Only debugging:
+  #  $stderr.puts arrival_times.inspect
   texts_for_sign = []
   arrival_times_text = arrival_times.each do |route, predictions|
     # Show first two predictions
-    prediction_text = predictions.slice(0,2).map(&:muni_time).join(' & ')
+    prediction_text = predictions.slice(0, 2).map{|p| muni_time(p.time)}.join(' & ')
     unless prediction_text.empty?
       # Fixup route name.
       route_name = fixup_route_name(route, predictions[0])
@@ -71,6 +93,8 @@ def update_sign(font, options)
     text_for_sign = font.render_multiline(["No routes", "until next morning."], 8, :ignore_shift_h => true, :distance => 0, :fixed_width => LED_Sign::SCREEN_WIDTH).zero_one
   end
   LED_Sign.pic(text_for_sign)
+#  puts text_for_sign
+#  $stdout.flush
 end
 
 while true
--- client/lowlevel.pl
+++ client/lowlevel.pl
@@ -12,62 +12,63 @@ use strict qw(subs vars);
 use warnings;
 use Getopt::Long;
 use Device::MiniLED;
-my $sign=Device::MiniLED->new(devicetype => "sign");
+my $sign = Device::MiniLED->new(devicetype => "sign");
 
 my $type = 'text';
 my $speed = 1;
 my $effect = 'hold';
 
 my $options_result = GetOptions(
-  'type=s' => \$type,
-  'speed=i' => \$speed,
-  'effect=s' => \$effect,
+    'type=s'   => \$type,
+    'speed=i'  => \$speed,
+    'effect=s' => \$effect,
 );
 
 my $height = 0;
 my $data = '';
 my @messages = ();
 while (<STDIN>) {
-  chomp;
-  # Don't let perl treat a string of a single 0 as false!
-  if (length($_) > 0) {
-    $height ++;
-    $data .= $_;
-  }
-  if (length($_) == 0 or eof(STDIN)) {
-    # Add message if we have some.
-    push @messages, {data => $data, height => $height} if length($data);
-    $height = 0;
-    $data = '';
-  }
+    chomp;
+    # Don't let perl treat a string of a single 0 as false!
+    if (length($_) > 0) {
+        $height++;
+        $data .= $_;
+    }
+    if (length($_) == 0 or eof(STDIN)) {
+        # Add message if we have some.
+        push @messages, { data => $data, height => $height } if length($data);
+        $height = 0;
+        $data = '';
+    }
 }
 
 for my $message_data (@messages) {
-  if ($type eq 'pic') {
-    my $data = $message_data->{data};
-    my $height = $message_data->{height};
-    $data =~ s/[^01]//g;
-    my $width = int(length($data)/$height);
-    # This will verify if we have correct number of bits.
-    my $pic = $sign->addPix(
-      height => $height,
-      width => $width,
-      data => $data,
-    );
-    $sign->addMsg(
-      data => $pic,
-      effect => $effect,
-      # For multiple messages, the speed seems to control transition speed in
-      # multi-message mode.
-      speed => $speed,
-    );
-  } else {
-    $sign->addMsg(
-      data => $message_data->{data},
-      effect => (length($message_data->{data}) > 13) ? 'scroll' : 'hold',
-      speed => $speed,
-    );
-  }
+    if ($type eq 'pic') {
+        my $data = $message_data->{data};
+        my $height = $message_data->{height};
+        $data =~ s/[^01]//g;
+        my $width = int(length($data) / $height);
+        # This will verify if we have correct number of bits.
+        my $pic = $sign->addPix(
+            height => $height,
+            width  => $width,
+            data   => $data,
+        );
+        $sign->addMsg(
+            data   => $pic,
+            effect => $effect,
+            # For multiple messages, the speed seems to control transition speed in
+            # multi-message mode.
+            speed  => $speed,
+        );
+    }
+    else {
+        $sign->addMsg(
+            data   => $message_data->{data},
+            effect => (length($message_data->{data}) > 13) ? 'scroll' : 'hold',
+            speed  => $speed,
+        );
+    }
 }
 
 # #
@@ -91,4 +92,9 @@ for my $message_data (@messages) {
 #         effect => "scroll",
 #         speed => 3
 # );
-$sign->send(device => "/dev/ttyUSB0");
+$sign->send(
+    device      => "/dev/ttyUSB0"
+#    device      => "/dev/ttyUSB0",
+#    baudrate    => "9600",
+#    packetdelay => 0.5
+);
