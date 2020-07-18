457da08f34065b3c518d83dc4f1cb110004ff635
JP <jp@sutrosoftware.com>
2020-07-18 16:26:02 -0700

    small tweaks

--- client/checksign.sh
+++ client/checksign.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-   echo "Usage: checksign.sh <hostname>"
-   exit 1
+	echo "Usage: checksign.sh <hostname>"
+	exit 1
 fi
 
 if [ -f /home/pi/reset.lock ]; then
-   echo "found lock file" >> /home/pi/reset.log
-elif ping -c 1 $1 &> /dev/null; then
-   touch /home/pi/reset.lock
-   echo `date` " - starting reset" >> /home/pi/reset.log
-   /home/pi/muni-led-sign/client/clear.pl
-   sudo ifconfig wlan0 down
-   sleep 5
-   sudo ifconfig wlan0 up
-   echo `date` " - finished reset" >> /home/pi/reset.log
-   rm /home/pi/reset.lock
+	echo "found lock file" >> /home/pi/reset.log
+elif ping -q -c 1 $1 &> /dev/null; then
+	touch /home/pi/reset.lock
+	echo `date` " - starting reset" >> /home/pi/reset.log
+	sudo ifconfig wlan0 down
+	/home/pi/muni-led-sign/client/clear.pl
+	sudo ifconfig wlan0 up
+	echo `date` " - finished reset" >> /home/pi/reset.log
+	rm /home/pi/reset.lock
 fi
